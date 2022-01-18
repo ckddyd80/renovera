58a8dd01e79733f91952fae0cef11756f5140015
ckddyd80 <jungcy1213@naver.com>
2022-01-18 18:43:25 +0900

    renovera02

---
+++ js/common.js
@@ -0,0 +1,6 @@
+    /* nav 메뉴에 마우스 올렸을때 */
+    $('.gnb_menu .nav li').hover(function() {
+        $('#gnb_sub_container').show();
+      }, function(){
+        $('#gnb_sub_container').hide();
+      });

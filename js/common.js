b8492c517b21f656175531115218ce73099db8f6
ckddyd80 <jungcy1213@naver.com>
2022-01-19 18:40:45 +0900

    renovera04

--- js/common.js
+++ js/common.js
@@ -1,6 +1,16 @@
-    /* nav 메뉴에 마우스 올렸을때 */
-    $('.gnb_menu .nav li').hover(function() {
-        $('#gnb_sub_container').show();
-      }, function(){
-        $('#gnb_sub_container').hide();
-      });
+/* nav 메뉴에 마우스 올렸을때 */
+// $(".gnb_menu .nav li").on({
+//     "mouseenter" : function() {
+//         $('#gnb_sub_container').show();
+//     }
+// });
+
+
+//   /* 하위 메뉴 안보이게 하기 */
+
+//   $("#gnb_sub_container").on({
+//     "mouseleave" : function() {
+//       $('#gnb_sub_container').hide();
+//     }
+    
+// });

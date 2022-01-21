051395d5df421af2dd8051a303ada7c909f79769
ckddyd80 <jungcy1213@naver.com>
2022-01-21 18:42:15 +0900

    renovera06

--- js/common.js
+++ js/common.js
@@ -5,24 +5,74 @@ $(".gnb_menu .nav li").on({
     }
 });
 
-
-/* 액티브 바 show */
-
-$(".active_bar").show();
-
-// 선택된 li의 left와 width를 구해 active_bar의 위치와 길이를 조절한다
-var listLeft = $(this).offset().left550;
-var listWidth = $(this).width();
-
-$(".active_bar").width(listWidth);
-$(".active_bar").offset({left: listLeft + 52}); // li에 padding이 있었으로 17을 더 더해준다.
-
-//   /* 하위 메뉴 안보이게 하기 */
-
 $("#gnb_sub_container").on({
     "mouseleave" : function() {
         $('#gnb_sub_container').hide();
     }
     
 });
+
+    /* 모바일 햄버거 메뉴 클릭시*/
+
+    $(".trigger").on({  
+        "click" : function(){
+            // 햄버거 메뉴 버튼이 보일때 (햄버거 메뉴가 visible 상태인가?로 판단) 
+            if($(".mobile.hamburger").is(":visible")) {
+                /* 모바일 닫기 버튼 show */
+                $(".mobile.hamburger").hide();
+                $(".mobile.close").show();
+
+                /* 모바일 전체 메뉴 show */
+                $("#main_header").height(60);
+                $("#main_content").hide();
+                $("#main_footer").hide();
+
+                $("#mobile_menu").empty(); // #mobile_menu 하위 태그 초기화
+                $(".nav li").eq(3).show();    // mobile에서만 보이는 메뉴 2개 추가
+                $(".nav li").eq(4).show();
+
+                var nav = $(".nav").clone(); 
+                $("#mobile_menu").append(nav);
+                $("#mobile_menu").show();
+                
+            } else {
+                // 닫기 메뉴 버튼이 보일때
+                /* 모바일 햄버거 버튼 show */
+                $(".mobile.hamburger").show();
+                $(".mobile.close").hide();
+
+                /* 모바일 전체 메뉴 show */
+                $("#main_header").height(390);
+                $("#main_header #search_box").show();
+                $("#main_content").show();
+                $("#main_footer").show();
+                $("#mobile_menu").hide();
+
+            }
+        
+        }
+    });
+
+    $(window).resize(function() {
+        var width = $(window).width();
+    
+        if(width >= 767) {
+            $(".nav li").eq(3).hide();   
+            $(".nav li").eq(4).hide();
+        }
+
+        if ($("#mobile_menu").is(":visible")) {
+            $(".mobile.hamburger").show();
+            $(".mobile.close").hide();
+
+            $("#main_header").height(390);
+            $("#main_header #search_box").show();
+            $("#main_content").show();
+            $("#main_footer").show();
+            $("#mobile_menu").hide();
+        }
+    
+    });
+
+
 
